dcb223703f9fdd2d32445ebb5db91a74408d0646
biraj9509 <34903544+biraj9509@users.noreply.github.com>
2018-09-26 00:38:03 -0500

    changes made on signup login and routing

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,22 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
+
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
 import { ValueComponent } from './value/value.component';
+import { LogComponent } from './log/log.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+
+const appRoutes: Routes = [
+
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'log', component: LogComponent },
+  { path: 'sinup', component: SignupComponent },
+];
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    LogComponent,
+    HomeComponent,
+    SignupComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    )
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+    LogComponent
+  ]
 })
+
 export class AppModule { }
+
+
